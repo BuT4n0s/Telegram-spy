c83340f6aba7da848ae7894fa71258033e81b87c
BuT4n0s <72738808+BuT4n0s@users.noreply.github.com>
2021-03-17 14:38:36 +0100

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,5 +1,5 @@
-# A token you get from the Telegram's botfather
-telegrambot = '1341936476:AAEoI2821W3G0Ji23qIwjV3aI16qQGwvbQ0'
+# A token you get from the Telegram's botfather  
+telegrambot = ''  
 
-# A chat_id of your client
-adminId = [467907420, 84070351]
+# A chat_id of your client  
+adminId = []  
